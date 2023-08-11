10498a40938a18b0f0829054aa1cf555646987eb
Ling Wang <amyling.wang@gmail.com>
2023-08-12 00:11:41 +0200

    add new note to an array

--- keeper-app-part-3-starting/src/components/App.jsx
+++ keeper-app-part-3-starting/src/components/App.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const notes=[{}];
+function buttenHandel(title, content){
+    console.log(title)
+    console.log(content)
+    const note ={"title": title, "content":content}
+    notes.push(note);
+    console.log(notes)
+    event.preventDefault();
+}
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea click={buttenHandel}/>
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- keeper-app-part-3-starting/src/components/CreateArea.jsx
+++ keeper-app-part-3-starting/src/components/CreateArea.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const[noteTitle, setNoteTitle] = useState("");
+  const[noteContent, setNoteContent] = useState("")
+  
+
+  function handleInputChange(){
+    const titleText = event.target.value;
+    setNoteTitle(titleText);
+  }
+
+  function handleContentChange(){
+    const contentText = event.target.value;
+    setNoteContent(contentText);
+  }
+
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form >
+        <input onChange={handleInputChange} name="title" placeholder="Title" value={noteTitle} />
+        <textarea onChange={handleContentChange} name="content" placeholder="Take a note..." rows="3" value={noteContent} />
+        <button onClick={()=>{props.click(noteTitle, noteContent)}}>Add</button>
       </form>
     </div>
   );
