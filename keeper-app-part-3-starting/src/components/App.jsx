c12d10dab767209ef9ce6e33f70b0edaa7114761
Ling Wang <amyling.wang@gmail.com>
2023-08-14 15:10:35 +0200

    Last commit

--- keeper-app-part-3-starting/src/components/App.jsx
+++ keeper-app-part-3-starting/src/components/App.jsx
@@ -4,21 +4,31 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+
 function App() {
-  const notes=[{}];
-function buttenHandel(title, content){
-    console.log(title)
-    console.log(content)
+  const [notes, setNotes]=useState([]);
+
+function buttenHandel(event, title, content){
+  event.preventDefault();
+    //console.log(title)
+    //console.log(content)
     const note ={"title": title, "content":content}
-    notes.push(note);
-    console.log(notes)
-    event.preventDefault();
+    setNotes((prevNotes)=>[...prevNotes, note]);
+    //console.log(notes) 
+   
+}
+
+function deleteNote(id){
+
+ setNotes((prevNotes)=>{return prevNotes.filter((note, index)=>{return (index!==id)})})
+
+
 }
   return (
     <div>
       <Header />
       <CreateArea click={buttenHandel}/>
-      <Note key={1} title="Note title" content="Note content" />
+      {notes.map((note, index)=> (<Note key={index} id={index} title={note.title} content={note.content} deleteButten={deleteNote} />))}
       <Footer />
     </div>
   );
--- keeper-app-part-3-starting/src/components/CreateArea.jsx
+++ keeper-app-part-3-starting/src/components/CreateArea.jsx
@@ -6,12 +6,12 @@ function CreateArea(props) {
   const[noteContent, setNoteContent] = useState("")
   
 
-  function handleInputChange(){
+  function handleInputChange(event){
     const titleText = event.target.value;
     setNoteTitle(titleText);
   }
 
-  function handleContentChange(){
+  function handleContentChange(event){
     const contentText = event.target.value;
     setNoteContent(contentText);
   }
@@ -22,7 +22,7 @@ function CreateArea(props) {
       <form >
         <input onChange={handleInputChange} name="title" placeholder="Title" value={noteTitle} />
         <textarea onChange={handleContentChange} name="content" placeholder="Take a note..." rows="3" value={noteContent} />
-        <button onClick={()=>{props.click(noteTitle, noteContent)}}>Add</button>
+        <button onClick={()=>{props.click( event, noteTitle, noteContent); setNoteTitle(""); setNoteContent("")}}>Add</button>
       </form>
     </div>
   );
